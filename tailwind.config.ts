9d85a8a62083b0bf25141b3a1215a6e6b8c638fc
Andrew Oplas <andrewmoplas@gmail.com>
2024-04-23 01:11:18 +0800

    add shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,67 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      animation: {
-        text: "text 5s ease infinite",
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        foreground: "hsl(var(--foreground))",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        accent: "var(--color-accent)",
+        background: "var(--color-background)",
+        "t-primary": "var(--color-text-primary)",
+        "t-secondary": "var(--color-text-secondary)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+        button: "10px",
+        card: "24px",
       },
       keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
         text: {
           "0%, 100%": {
             "background-size": "200% 200%",
@@ -23,10 +73,22 @@ const config: Config = {
           },
         },
       },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        text: "text 5s ease infinite",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      boxShadow: {
+        cards: "0px 50px 100px -20px rgba(0, 0, 0, .15)",
+      },
+      fontFamily: {
+        heading: ["var(--font-family-heading)"],
+        body: ["var(--font-family-body)"],
       },
       fontSize: {
         h1: "var(--font-h1)",
@@ -38,48 +100,22 @@ const config: Config = {
         body: "var(--font-body)",
         "body-large": "var(--font-body-large)",
       },
-      fontFamily: {
-        heading: ["var(--font-family-heading)"],
-        body: ["var(--font-family-body)"],
-      },
-      borderRadius: {
-        button: "10px",
-        card: "24px",
-      },
-      colors: {
-        primary: "var(--color-primary)",
-        secondary: "var(--color-secondary)",
-        accent: "var(--color-accent)",
-        background: "var(--color-background)",
-        "t-primary": "var(--color-text-primary)",
-        "t-secondary": "var(--color-text-secondary)",
-
-        textBold: "var(--color-text-bold)",
-        appBlue: "var(--color-blue)",
-        appLightblue: "var(--color-lightblue)",
-        appLightgrey: "var(--color-lightgrey)",
-        appMint: "var(--color-mint)",
-        appLilac: "var(--color-lilac)",
-        appRust: "var(--color-rust)",
-        appBabyblue: "var(--color-babyblue)",
-        appPink: "var(--color-pink)",
-      },
-      boxShadow: {
-        cards: "0px 50px 100px -20px rgba(0, 0, 0, .15)",
-      },
-      gridTemplateColumns: {
-        "32": "repeat(32, minmax(0, 1fr))",
-      },
       gridColumn: {
         "span-13": "span 13 / span 13",
         "span-14": "span 14 / span 14",
         "span-15": "span 15 / span 15",
       },
+      gridTemplateColumns: {
+        "32": "repeat(32, minmax(0, 1fr))",
+      },
       maxWidth: {
         section: "1600px",
+        "post-content": "900px",
+        "post-images": "1200px",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
